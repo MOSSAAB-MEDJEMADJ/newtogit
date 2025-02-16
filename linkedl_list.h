ebb8ab83b176268b2502af5567ae1cb05bbcf361
MOSSAAB <mossaabmedj@gmail.com>
2025-02-16 18:41:29 +0100

    Implement addNode and add head node

--- linkedl_list.h
+++ linkedl_list.h
@@ -8,11 +8,24 @@ typedef struct node {
     struct node *next;
 } node;
 
+node *head = NULL;
+
 node *createNode(int data) {
     node *n = (node*)malloc(sizeof(node));
     n->data = data;
     n->next = NULL;
     return n;
+}
+
+void addNode(int data) {
+    node *n = createNode(data);
+    if (head == NULL) {
+        head = n;
+        head->next = NULL;
+    } else {
+        n->next = head;
+        head = n;
+    }
 }
 
 #endif
